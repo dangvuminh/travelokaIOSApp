b6d0aab4f1be1b8ce2da2ad2ed2e697969e56201
dangvuminh <dangvuminh98@gmail.com>
2024-01-25 21:40:51 +0700

    Look flight pannel + link destination for category flight

--- traveloka/components/products/CategorySection/MainCategories.swift
+++ traveloka/components/products/CategorySection/MainCategories.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct MainCategories: View {
-    
+    @State var clickItem: Int? = 0
     func CategoryItem(icon: String, name: String) -> some View {
-        VStack {
-            Image(systemName: icon).resizable()
-                .frame(width: 35.0, height: 35.0)
-            Text(name).font(.system(size: 15.0))
-        }
+            VStack {
+                Image(systemName: icon).resizable()
+                    .frame(width: 35.0, height: 35.0)
+                Text(name).font(.system(size: 15.0))
+            }
     }
     
     var body: some View {
         HStack {
             Spacer()
-            CategoryItem(icon: "airplane.circle", name: "Flights")
+            NavigationLink(
+                destination: LookupFlightPage(),
+                tag: 1,
+                selection: $clickItem) {
+                CategoryItem(icon: "airplane.circle", name: "Flights").onTapGesture {
+                    clickItem = 1
+                }
+            }
             Spacer()
             Spacer()
             CategoryItem(icon: "building.2", name: "Hotels")
---
+++ traveloka/components/products/SearchPannel/FlightSearchPannel.swift
@@ -0,0 +1,55 @@
+//
+//  Flights.swift
+//  traveloka
+//
+//  Created by macbook pro on 25/01/2024.
+//
+
+import SwiftUI
+
+struct FlightSearchPannel: View {
+    @State var isReturned = false
+    var body: some View {
+        VStack {
+            ZStack {
+                BottomLineControl(label: "From", icon: {
+                    Image(systemName: "airplane")
+                }, searchText: "Singapore (SIN)")
+                Image(systemName: "arrow.up.arrow.down.circle.fill").resizable().frame(width: 30, height: 30).padding(EdgeInsets(.init(top: 25, leading: 300.0, bottom: 0, trailing: 0))).foregroundColor(.blue)
+            }
+            BottomLineControl(label: "To", icon: {
+                Image(systemName: "airplane")
+            }, searchText: "Beijing (BEIA)")
+            
+            ZStack {
+                BottomLineControl(label: "Departure time", icon: {
+                    Image(systemName: "calendar")
+                }, searchText: "02/02/2024")
+                VStack {
+                    Text("Return?").padding([.leading], 250).font(.system(size: 13.0))
+                    Toggle("", isOn: $isReturned).padding([.bottom,.trailing], 45)
+                }
+            }
+            BottomLineControl(label: "Return time", icon: {
+                Image(systemName: "calendar")
+            }, searchText: "04/02/2024")
+            HStack {
+                BottomLineControl(label: "Number of passengers", icon: {
+                    Image(systemName: "person.2")
+                }, searchText: "1 passenger", height: 80, width: 160)
+                BottomLineControl(label: "Seat type", icon: {
+                    Image(systemName: "studentdesk")
+                }, searchText: "1 passenger", height: 70, width: 160)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Search").foregroundColor(.white)
+            }).frame(width: 350, height: 40, alignment: .center).background(Color.init(red: 1, green: 0.38, blue: 0.27)).cornerRadius(5.0)
+        }
+    }
+}
+
+struct FlightSearchPannel_Previews: PreviewProvider {
+    static var previews: some View {
+        FlightSearchPannel()
+    }
+}
